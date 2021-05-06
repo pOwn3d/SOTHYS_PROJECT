f7e95a8e31b17171cad0e1b70b240d4d5f492ac3
Christophe <clopez@nexton-group.com>
2021-05-06 17:16:51 +0200

    fix array push csv export

--- src/Services/CsvExporter.php
+++ src/Services/CsvExporter.php
@@ -25,17 +25,18 @@ class CsvExporter
     public function exportCoedi()
     {
         $orders = $this->orderRepository->findOrderCustomerExport();
-
+        $rows   = [];
         foreach ($orders as $order) {
 
+            array_push($rows, [ 'E|' . $order->getId() . '|310262|20201214|20201225|VIR60DDF|2|3|3|L01|||||||||COEDI||2|Urgent Order|310262|9|EXW|USSAC||' ]);
             $orderLines = $this->orderLineRepository->findByOrderID($order->getId());
-            $rows       = [ [ 'E|' . $order->getId() . '|310262|20201214|20201225|VIR60DDF|2|3|3|L01|||||||||COEDI||2|Urgent Order|310262|9|EXW|USSAC||' ] ];
 
             foreach ($orderLines as $orderLine) {
                 array_push($rows, [ 'L|' . $orderLine->getItemID()->getId() . '|' . $orderLine->getQuantity() . '|' . $orderLine->getPrice() . '|Valeur1 remise/frais|Prix net|Motif gratuit|ID COEDI|Projet|Valeur2 remise/frais' ]);
             }
 
         }
+
 
         $writer = Writer::createFromFileObject(new SplTempFileObject());
         $writer->insertAll($rows); //using an array
