c4aef83f12254c4d9131186e7affab6012a94ca3
Christophe <compt3@gmail.com>
2021-05-28 15:34:07 +0200

    update promo clean cart to add other promo

--- src/Services/PromoServices.php
+++ src/Services/PromoServices.php
@@ -6,6 +6,7 @@ namespace App\Services;
 
 use App\Entity\OrderDraft;
 use App\Repository\ItemRepository;
+use App\Repository\OrderDraftRepository;
 use App\Repository\PromotionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Security;
@@ -29,12 +30,17 @@ class PromoServices
      * @var \Doctrine\ORM\EntityManagerInterface
      */
     private EntityManagerInterface $em;
+    /**
+     * @var \App\Entity\OrderDraft
+     */
+    private OrderDraftRepository $orderDraftRepository;
 
-    public function __construct(PromotionRepository $promotionRepository, ItemRepository $itemRepository, Security $security, EntityManagerInterface $em)
+    public function __construct(PromotionRepository $promotionRepository, ItemRepository $itemRepository, Security $security, EntityManagerInterface $em, OrderDraftRepository $orderDraftRepository)
     {
         $this->promotionRepository = $promotionRepository;
         $this->itemRepository = $itemRepository;
         $this->security = $security;
+        $this->orderDraftRepository = $orderDraftRepository;
 
         $this->em = $em;
     }
@@ -43,6 +49,16 @@ class PromoServices
     {
 
         $promo = $this->promotionRepository->findOneBy(["id" => $id]);
+        $cart = $this->orderDraftRepository->findOrderDraftSocietyPromo($this->security->getUser()->getSocietyID());
+
+
+        if (!empty($cart)){
+            $this->em->remove($cart['0']);
+            $this->em->flush();
+        }
+
+
+
         $item = $this->itemRepository->findOneBy(["id" => $promo->getPromotionItem()->getValues()[0]->getItem()->getId()]);
         $product = $promo->getPromotionItem()->getValues()[0]->getPrice();
 
