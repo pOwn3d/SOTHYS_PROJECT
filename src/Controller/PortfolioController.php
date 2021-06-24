c09ade7cd39dfd822405688dca1a0c7898704adf
DFA <damien.fayol@sothys.net>
2021-06-24 17:30:08 +0200

    Add basic menu structure

---
+++ src/Controller/PortfolioController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PortfolioController extends AbstractController
+{
+    /**
+     * @Route("/{_locale}/portfolio", name="app_portfolio")
+     */
+    public function index(): Response
+    {
+        return $this->render('portfolio/index.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+}
